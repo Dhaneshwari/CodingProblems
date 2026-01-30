40e98cc11a161b6e5e9fa0a0355cb58e3e7859ac
sahul <dsdhaneshwari1@gmail.com>
2026-01-30 11:03:21 +0530

    initial commit

---
+++ src/main/java/com/techie/factors/PrimeFactor.java
@@ -0,0 +1,19 @@
+package com.techie.factors;
+
+public class PrimeFactor {
+    public static void main(String[] args) {
+        int n=24;
+        System.out.println(calculatePrimeFactor(n));
+    }
+
+    public static int calculatePrimeFactor(int n){
+        int count = 0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                return       count++;
+            }
+            return    count+=2;
+        }
+        return -1;
+    }
+}
